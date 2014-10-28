0403c6c5a6883937c95e7feddc0c3e11db91b93f
Ricardo Vazquez <ricardo@mozillafoundation.org>
2014-10-28 15:56:32 -0400

    Changing font and adding Fittext.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
 $(function() {
 	'use strict';
-	$('.hero-unit article').eq(0).hide().delay(500).fadeIn(1000);
-	$('.hero-unit article').eq(1).hide().delay(1500).fadeIn(1000);
-	$('.hero-unit article').eq(2).hide().delay(2500).fadeIn(1000);
-	$('.hero-unit article').eq(3).hide().delay(3500).fadeIn(1000);
-	// $(".hero-unit article").fitText(1.7, {
-	// 	minFontSize: "20px"
-	// });
-	// $(".heading").fitText(0.9);
+	$('body').css('opacity', '0').fadeTo(1500, 1,'swing');
+
+	var $introLine = $('.hero-unit article');
+
+	// $introLine.eq(0).css('opacity', '0.2').fadeTo(1350, 1,'swing');
+	// $introLine.eq(1).css('opacity', '0.2').delay(1000).fadeTo(1350, 1,'swing');
+	// $introLine.eq(2).css('opacity', '0.2').delay(2000).fadeTo(1350, 1,'swing');
+	// $introLine.eq(3).css('opacity', '0.2').delay(3000).fadeTo(1350, 1,'swing');
+
+	// $('.hero-unit article').eq(1).delay(5000).addClass('visible');
+	// $('.hero-unit article').eq(2).delay(7000).addClass('visible');
+	// $('.hero-unit article').eq(2).delay(9000).addClass('visible');
+
+	// $('.hero-unit article').eq(1).hide().delay(1500).fadeIn(1000);
+	// $('.hero-unit article').eq(2).hide().delay(2500).fadeIn(1000);
+	// $('.hero-unit article').eq(3).hide().delay(3500).fadeIn(1000);
+	$(".hero-unit article").fitText(1.5, {
+		minFontSize: "65px"
+	});
 	$('nav').delay(2000).addClass('visible');
 });
 
